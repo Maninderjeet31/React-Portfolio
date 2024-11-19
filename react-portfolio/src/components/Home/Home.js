5b368aac881f186df5793a6bbb1f74efed37a1e2
maninderjeet.singhibm.com <mani.pabla31@gmail.com>
2024-11-19 15:08:18 -0700

    typewriter effect

--- react-portfolio/src/components/Home/Home.js
+++ react-portfolio/src/components/Home/Home.js
@@ -2,6 +2,11 @@ import './Home.css';
 
 const Home = () => {
 
+    const style = {
+        '--n': 7,    // Custom property n
+        '--d': '1.5s' // Custom property d
+      };
+
     return (
         <>
             <section id="home" class="homeDiv ">
@@ -9,7 +14,19 @@ const Home = () => {
 				<div class="homeText">
 					<div class="intro">
                         <h1 class="main-text">Maninderjeet Singh</h1>
-						<p class="sub-text">I'm a developer !</p>
+						<p class="sub-text">
+                            I am <span class="type" style={style}>
+                            <span>
+                                <span>a developer!</span>
+                                <span>a designer!</span>
+                                <span>a coder!</span>
+                                <span>a problem solver!</span>
+                                <span>a creative thinker!</span>
+                                <span>a team player!</span>
+                                <span>a quick learner!</span>
+                            </span>
+                            </span>
+                        </p>
 						<button type="button" onclick="window.location.href='#experience'">Experience</button> &nbsp; &nbsp;
 						<button type="button">Let's Connect</button>
 					</div>
